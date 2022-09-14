5b2caecac7de319c839ed1abcb8078940f5aa25b
ErenC37 <80468546+ErenC37@users.noreply.github.com>
2022-09-14 17:26:59 +0300

    Skor Sistemi
    Best Score Sistemi Silindi

--- Assets/Scripts/Scores.cs
+++ Assets/Scripts/Scores.cs
@@ -3,22 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
-public class BestScoreData
-{
-    public int score = 0;
-}
-
 public class Scores : MonoBehaviour
 {
     public Text socreText;
 
-    
-    private BestScoreData bestScores_ = new BestScoreData();
     private int currentScores_;
-
-    private string bestScoreKey_ = "bsdat";
-
 
     void Start()
     {
@@ -40,10 +29,7 @@ public class Scores : MonoBehaviour
 
     private void AddScores(int socres)
     {
-        currentScores_ += socres;
-        if(currentScores_ > bestScores_.score)
-            bestScores_.score = currentScores_;
-        
+        currentScores_ += socres;  
         UpdateScoreText();
     }
 
